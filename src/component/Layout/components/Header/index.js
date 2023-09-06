ba9cecd3a7f68ee8330fa4f20ba5c4da8995ea71
vanhung591 <96800654+vanhung591@users.noreply.github.com>
2023-09-06 23:35:34 +0700

    Building UI Header 5

--- src/component/Layout/components/Header/index.js
+++ src/component/Layout/components/Header/index.js
@@ -26,6 +26,19 @@ const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
         title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    code: 'vi',
+                    title: 'Viet',
+                },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -43,6 +56,10 @@ function Header() {
     useEffect(() => {
         setSearchResult([]);
     }, []);
+    const handleMenuChange = (MenuItem) => {
+        console.log(MenuItem);
+    };
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
@@ -83,7 +100,7 @@ function Header() {
                     <Button text>Upload</Button>
                     <Button primary>Log in</Button>
 
-                    <Menu items={MENU_ITEMS}>
+                    <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
                         <button className={cx('more-btn')}>
                             <FontAwesomeIcon icon={faEllipsisVertical} />
                         </button>
